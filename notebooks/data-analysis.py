b03895f23052d6a722ff7bc53a06aac0731426c0
vlandeau <vlandeau@gmail.com>
2025-08-23 19:46:40 +0200

    chore: clean financial data, check coherence and aggregate per quarter

--- notebooks/data-analysis.py
+++ notebooks/data-analysis.py
@@ -20,77 +20,121 @@ import plotly.express as px
 
 # %%
 # %matplotlib inline
-load_dotenv()
 current_path = Path.cwd()
+data_path = current_path / ".." / "data"
 
 # %% [markdown]
 # # Data loading
 
 # %%
-pnl_df = pd.read_excel(current_path / ".." / "data" / "P&L.xlsx", index_col=0, skiprows=[1])
+pnl_df = pd.read_excel(data_path / "P&L.xlsx", skiprows=[1])
 
 # %%
 pnl_df
 
+
 # %%
-pnl_df.index
+def extract_kpis_df(first_row_index: int = 0, last_row_index: int = len(pnl_df)) -> pd.DataFrame:
+    kpis_df = pnl_df[first_row_index:last_row_index].set_index("Unnamed: 0").T
+    kpis_df.columns.name = None
+    kpis_df.index = pd.to_datetime(kpis_df.index, format='%b %y')
+    return kpis_df.fillna(0)
+
+
+# %%
+transactions_df = extract_kpis_df(last_row_index=10)
+transactions_df
+
+# %%
+revenue_df = extract_kpis_df(10, 20)
+revenue_df
+
+# %%
+services_cost_df = extract_kpis_df(20, 30)
+services_cost_df
+
+# %%
+credit_card_fees_df = extract_kpis_df(30, 40)
+credit_card_fees_df
+
+# %%
+gross_profit_df = extract_kpis_df(40, 50)
+gross_profit_df
+
+# %%
+opex_df = extract_kpis_df(first_row_index=50, last_row_index=56)
+opex_df
+
+# %%
+ebitda_df = extract_kpis_df(first_row_index=56, last_row_index=57)
+ebitda_df
 
 # %% [markdown]
-# # Data exploration
+# # Data coherence check
 
 # %%
-kpis = ["Staff Costs", "Total OPEX", "EBITDA"]
-px.line(pnl_df.loc[kpis].T)
+transactions = "Transactions"
+segments = list(set(transactions_df.columns) - {transactions})
+segments
 
 # %%
-px.line((pnl_df.loc[kpis].T - pnl_df.loc[kpis].T.shift(12)).dropna())
+epsilon = 0.000001
+assert (transactions_df[transactions] - transactions_df[segments].sum(axis=1) < epsilon).all()
 
 # %%
-import numpy as np
-
-ebitda_calc = (
-    pnl_df.loc['Total Revenue']
-  - pnl_df.loc['Cost of Services']
-  - pnl_df.loc['Credit Card Fees']
-  - pnl_df.loc['Total OPEX']
-)
-diff = pnl_df.loc['EBITDA'] - ebitda_calc
-ok = np.allclose(diff.values, 0, equal_nan=True)
-ok
+total_revenue = "Total Revenue"
+assert (revenue_df[total_revenue] - revenue_df[segments].sum(axis=1) < epsilon).all()
 
 # %%
-pnl_df.loc['EBITDA'] - ebitda_calc
+cost_of_services = "Cost of Services"
+assert (services_cost_df[cost_of_services] - services_cost_df[segments].sum(axis=1) < epsilon).all()
 
 # %%
-import pandas as pd
+credit_card_fees = "Credit Card Fees"
+assert (credit_card_fees_df[credit_card_fees].fillna(0) - credit_card_fees_df[segments].fillna(0).sum(axis=1) < epsilon).all()
 
-headers = {'Transactions','Total Revenue','Cost of Services','Credit Card Fees','Gross Profit'}
-opex = {'Business Operations','Customer Service','Marketing Expenses','Staff Costs','Other OPEX','Total OPEX'}
+# %%
+gross_profit = "Gross Profit"
+assert (gross_profit_df[gross_profit] - gross_profit_df[segments].sum(axis=1) < epsilon).all()
 
-rows = []
-current = None
-for lab in pnl_df.index:
-    if lab in headers:
-        current = lab
-        rows.append((lab, 'TOTAL'))     # la ligne d’agrégat si elle existe
-    elif lab in opex:
-        rows.append(('OPEX', lab))
-    elif lab == 'EBITDA':
-        rows.append(('P&L', 'EBITDA'))
-    else:
-        rows.append((current, lab))     # lignes segmentées
+# %%
+total_opex = "Total OPEX"
+opex_dimensions = list(set(opex_df.columns) - {total_opex})
+assert (opex_df[total_opex] - opex_df[opex_dimensions].sum(axis=1) < epsilon).all()
 
-df2 = pnl_df.copy()
-df2.index = pd.MultiIndex.from_tuples(rows, names=['Section','Item'])
+# %%
+ebitda = "EBITDA"
+estimated_ebitda_df = gross_profit_df[gross_profit] + opex_df[total_opex]
+assert (ebitda_df[ebitda] - estimated_ebitda_df < epsilon).all()
 
-segments = ['Enterprise','Mid Market','A1','Marketplaces','Small+','Small','Local','Editions','Other']
-rev_tot  = df2.loc[('Total Revenue', segments)].sum()          # somme des lignes segments
-cos_tot  = df2.loc[('Cost of Services', segments)].sum()
-fees_tot = df2.loc[('Credit Card Fees', segments)].sum()
-gross_from_seg = rev_tot - cos_tot - fees_tot
 
-opex_tot = df2.loc[('OPEX','Total OPEX')]
-ebitda_from_seg = gross_from_seg - opex_tot
+# %% [markdown]
+# # Aggregation per quarter
 
+# %%
+def aggregate_per_quarter(df: pd.DataFrame) -> pd.DataFrame:
+    df_sampled_per_quarter = df.resample('QE', label='right').sum()
+    return df_sampled_per_quarter
+
+
+# %%
+transactions_per_quarter_df = aggregate_per_quarter(transactions_df)
+revenue_per_quarter_df = aggregate_per_quarter(revenue_df)
+cost_of_services_per_quarter_df = aggregate_per_quarter(services_cost_df)
+credit_card_fees_per_quarter_df = aggregate_per_quarter(credit_card_fees_df)
+gross_profit_per_quarter_df = aggregate_per_quarter(gross_profit_df)
+opex_per_quarter_df = aggregate_per_quarter(opex_df)
+ebitda_per_quarter_df = aggregate_per_quarter(ebitda_df)
+
+# %%
+output_data_path = data_path / "output"
+
+transactions_per_quarter_df.to_parquet(output_data_path / "transactions.parquet")
+revenue_per_quarter_df.to_parquet(output_data_path / "revenue.parquet")
+cost_of_services_per_quarter_df.to_parquet(output_data_path / "cost_of_services.parquet")
+credit_card_fees_per_quarter_df.to_parquet(output_data_path / "credit_card_fees.parquet")
+gross_profit_per_quarter_df.to_parquet(output_data_path / "gross_profit.parquet")
+opex_per_quarter_df.to_parquet(output_data_path / "opex.parquet")
+ebitda_per_quarter_df.to_parquet(output_data_path / "ebitda.parquet")
 
 # %%
